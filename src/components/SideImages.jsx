6fb15e806b79b2a0b11e44301033cf2cf2949e37
ShroukMostafaAhmed <shroukmostafa0075@gmail.com>
2026-01-15 15:12:18 +0200

    test

--- src/components/SideImages.jsx
+++ src/components/SideImages.jsx
@@ -4,17 +4,17 @@ export default function SideImages() {
   return (
     <>
       <img
-        src="/Group.png"
+        src="/groups/Group.png"
         alt="Group Icon 1"
         className="absolute top-[42%] left-[4%] w-[120px] z-10"
       />
       <img
-        src="/Group (1).png"
+        src="/groups/Group (1).png"
         alt="Group Icon 2"
         className="absolute top-[47%] left-[8%] w-[120px] z-10"
       />
       <img
-        src="/Group (2).png"
+        src="/groups/Group (2).png"
         alt="Group Icon 3"
         className="absolute bottom-[4%] left-[5%] w-[120px] z-10"
       />
